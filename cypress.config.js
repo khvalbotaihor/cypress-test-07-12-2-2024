a2eb267ee8de61632f8f92a6981e83533ec1656c
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2024-12-07 19:47:03 +0200

    refactor

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } = require('cypress');
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
