b0c162a2e0aa9e9270f1e6f3a5f5795a89d18bf2
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2024-12-07 22:27:07 +0200

    checked all possible identifications

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -2,45 +2,31 @@
 
 describe('First test suite', () => {
 
-  describe('suite section', () => {
-
-    beforeEach('login', () => {
-
-    })
-
-    it('first test', () => {
-
-    })
-
-    it('second test', () => {
-
-    })
-  })
-
   it('first test', () => {
 
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    //by tag name
+    cy.get('input')
+    //by id
+    cy.get('#inputEmail1')
+    //by class name
+    cy.get('.input-full-width')
+    //by attribute name
+    cy.get('[fullwidth]')
+    //by attribute and value
+    cy.get('[placeholder="Email"]')
+    //by entire class value
+    cy.get('[class="input-full-width size-medium shape-rectangle"]')
+    //by 2 attributes
+    cy.get('[fullwidth][placeholder="Email"]')
+    //by tag attributes and by class
+    cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+    //by cypress testid
+    cy.get('[data-cy="imputEmail1"]')
   })
 
-  it('second test', () => {
 
-  })
-
-  it('third test', () => {
-
-  })
-})
-
-describe('Second test suite', () => {
-
-  it('first test', () => {
-
-  })
-
-  it('second test', () => {
-
-  })
-
-  it('third test', () => {
-
-  })
 })
