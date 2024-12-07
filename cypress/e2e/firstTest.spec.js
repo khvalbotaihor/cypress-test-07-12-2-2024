cb014c592596f67beb953930d01b001bce0e3748
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2024-12-08 00:44:39 +0200

    add chain checks

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -28,5 +28,32 @@ describe('First test suite', () => {
     cy.get('[data-cy="imputEmail1"]')
   })
 
+  it.only('second test', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    //theory
+    //get() - find elements on the page by locator globally
+    //find() - find child elements by locator
+    //contains() - find HTML text and by text and locator
+
+    cy.contains('Sign in')
+    cy.contains('[status="warning"]', 'Sign in')
+    cy.contains('nb-card', 'Horizontal form').find('button')
+    cy.contains('nb-card', 'Horizontal form').contains('Sign in')
+
+
+    // cypress chain and DOM
+    cy.get('#inputEmail3')
+      .parents('form')
+      .find('button')
+      .should('contain', 'Sign in')
+      .parents('form')
+      .find('nb-checkbox')
+      .click()
+
+  })
+
 
 })
