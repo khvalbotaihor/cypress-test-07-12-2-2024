ff5e9e12aec3d03fff149fbd432a8c6b6d52bcfb
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2024-12-07 21:01:40 +0200

    organize cypress test architecture

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } = require('cypress');
+const { defineConfig } = require('cypress');
 
-export default defineConfig({
+module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: false,
   e2e: {
+    baseUrl: "http://localhost:4200",
+    excludeSpecPattern: ["**/1-getting-started", "**/2-advanced-examples"],
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/firstTest.spec.js
@@ -0,0 +1,46 @@
+/// <reference types="cypress" />
+
+describe('First test suite', () => {
+
+  describe('suite section', () => {
+
+    beforeEach('login', () => {
+
+    })
+
+    it('first test', () => {
+
+    })
+
+    it('second test', () => {
+
+    })
+  })
+
+  it('first test', () => {
+
+  })
+
+  it('second test', () => {
+
+  })
+
+  it('third test', () => {
+
+  })
+})
+
+describe('Second test suite', () => {
+
+  it('first test', () => {
+
+  })
+
+  it('second test', () => {
+
+  })
+
+  it('third test', () => {
+
+  })
+})
