bd1e4d3ca14c8c7cf390261f24b3dcf88ed97476
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2024-12-08 20:24:05 +0200

    checkboxes and radio buttons

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,12 +1,21 @@
 /// <reference types="cypress" />
 
+const exp = require("constants")
+const { lab } = require("d3-color")
+
 describe('First test suite', () => {
 
-  it('first test', () => {
-
+  beforeEach('open page', () => {
     cy.visit('/')
     cy.contains('Forms').click()
     cy.contains('Form Layouts').click()
+  })
+
+  it('first test', () => {
+
+    // cy.visit('/')
+    // cy.contains('Forms').click()
+    // cy.contains('Form Layouts').click()
 
     //by tag name
     cy.get('input')
@@ -29,9 +38,9 @@ describe('First test suite', () => {
   })
 
   it('second test', () => {
-    cy.visit('/')
-    cy.contains('Forms').click()
-    cy.contains('Form Layouts').click()
+    // cy.visit('/')
+    // cy.contains('Forms').click()
+    // cy.contains('Form Layouts').click()
 
     //theory
     //get() - find elements on the page by locator globally
@@ -54,10 +63,10 @@ describe('First test suite', () => {
       .click()
   })
 
-  it.only('save subject of the command', () => {
-    cy.visit('/')
-    cy.contains('Forms').click()
-    cy.contains('Form Layouts').click()
+  it('save subject of the command', () => {
+    // cy.visit('/')
+    // cy.contains('Forms').click()
+    // cy.contains('Form Layouts').click()
 
     cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
     cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
@@ -78,5 +87,55 @@ describe('First test suite', () => {
       cy.wrap(usingTheGrid).find('[for="inputPassword2"]').should('contain', 'Password')
     })
   } )
+
+  it('extract text values', () => {
+
+    //1
+    cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+    //2
+    cy.get('[for="exampleInputEmail1"]').then(label => {
+      const labelText = label.text()
+      expect(labelText).to.equal('Email address')
+      cy.wrap(label).should('contain', 'Email address')
+    })
+
+    //3
+    cy.get('[for="exampleInputEmail1"]').invoke('text').then(text => {
+      expect(text).to.equal('Email address')
+    })
+    cy.get('[for="exampleInputEmail1"]').invoke('text').should('contain', 'Email address')
+
+    //4
+    cy.get('[for="exampleInputEmail1"]').invoke('attr', 'class').then(classValue => {
+      expect(classValue).to.equal('label')
+    })
+
+    //5 invoke property
+    cy.get('#exampleInputEmail1').type('test@gmail.com')
+    cy.get('#exampleInputEmail1').invoke('prop', 'value').should('contain', 'test@gmail.com').then(propertyValue => {
+      expect(propertyValue).to.equal('test@gmail.com')
+    })
+  })
+
+  it('radio button', () => {
+    cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(radioButtons => {
+      cy.wrap(radioButtons).eq(0).check({force: true}).should('be.checked')
+      cy.wrap(radioButtons).eq(1).check({force: true})
+      cy.wrap(radioButtons).eq(0).should('not.be.checked')
+      cy.wrap(radioButtons).eq(2).should('be.disabled')
+        })
+  })
+
+})
+
+it.only('checkboxes', () => {
+  cy.visit('/')
+  cy.contains('Modal & Overlays').click()
+  cy.contains('Toastr').click()
+
+  //cy.get('[type="checkbox"]').check({force:true})
+  cy.get('[type="checkbox"]').eq(0).click({force: true})
+  cy.get('[type="checkbox"]').eq(1).check({force: true})
 
 })
