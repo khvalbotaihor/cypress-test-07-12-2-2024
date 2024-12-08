4dded9a0d4e1b3b3da94e9a1feb5db5c61ad70a5
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2024-12-09 00:05:02 +0200

    added date picker

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -141,20 +141,38 @@ it('checkboxes', () => {
 })
 
 it.only('datepicker', () => {
+  function checkDate(numberOfDays){
+    let date = new Date()
+    date.setDate(date.getDate() + numberOfDays)
+    let futureDay = date.getDate()
+    let futureMonth = date.toLocaleDateString('en-US', {month: 'short'})
+    let futureYear = date.getFullYear()
+    let dateToAssert = `${futureMonth} ${futureDay}, ${futureYear}`
+
+    cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(value => {
+      console.log(`future month - ${futureMonth} and future year - ${futureYear} and ${value}`)
+      if(!value.includes(futureMonth) || !value.includes(futureYear)){
+        cy.get('[data-name="chevron-right"]').click()
+        checkDate(numberOfDays)
+      }else{
+        cy.get('.day-cell').not('bounding-month').contains(futureDay).click()
+      }
+    })
+    return dateToAssert;
+  }
+
+
   cy.visit('/')
   cy.contains('Forms').click()
   cy.contains('Datepicker').click()
 
-  let date = new Date()
-  date.setDate(date.getDate() + 5)
-  let futureDate = date.getDate()
-  let dateToAssert = `Dec ${futureDate}, 2024`
-
   cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
     cy.wrap(input).click()
-    cy.get('.day-cell').not('bounding-month').contains(futureDate).click()
+
+    const dateToAssert = checkDate(50)
     cy.wrap(input).invoke('prop', 'value').should('contains', dateToAssert)
     cy.wrap(input).should('have.value', dateToAssert)
+
   })
 
 
