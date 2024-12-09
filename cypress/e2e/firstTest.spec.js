82697f93b54c6327a27e9ac437487d1c8732a727
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2024-12-10 00:10:59 +0200

    working with web tables, example 1

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -204,6 +204,18 @@ it('lists and drop-downs', () => {
 })
 
 
-it.only('', () => {
-  
+it.only('web tables', () => {
+  cy.visit('/')
+  cy.contains('Tables & Data').click()
+  cy.contains('Smart Table').click()
+
+  //1
+  cy.get('tbody').contains('tr', 'Larry').then(tableRow => {
+    cy.wrap(tableRow).find('.nb-edit').click()
+    cy.wrap(tableRow).find('[placeholder="Age"]').clear().type(33)
+    cy.wrap(tableRow).find('.nb-checkmark').click()
+    cy.wrap(tableRow).find('td').eq(6).should('contain', '33')
+  })
+
+
 })
