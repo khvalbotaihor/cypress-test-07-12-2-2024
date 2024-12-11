d78a0945522ab0ee780279c23a5911f8f79e2a36
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2024-12-11 23:41:11 +0200

    web tables

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -209,13 +209,40 @@ it.only('web tables', () => {
   cy.contains('Tables & Data').click()
   cy.contains('Smart Table').click()
 
-  //1
+  //1 get the row by text
   cy.get('tbody').contains('tr', 'Larry').then(tableRow => {
     cy.wrap(tableRow).find('.nb-edit').click()
     cy.wrap(tableRow).find('[placeholder="Age"]').clear().type(33)
     cy.wrap(tableRow).find('.nb-checkmark').click()
     cy.wrap(tableRow).find('td').eq(6).should('contain', '33')
   })
+
+  //2 get row by index
+    cy.get('thead').find('.nb-plus').click()
+    cy.get('thead').find('tr').eq(2).then(tr => {
+      cy.wrap(tr).find('[placeholder="First Name"]').type('john')
+      cy.wrap(tr).find('[placeholder="Last Name"]').type('smith')
+      cy.wrap(tr).find('.nb-checkmark').click()
+
+    })
+
+    cy.get('tbody tr').first().find('td').then(tc => {
+      cy.wrap(tc).eq(2).should('contain', 'john')
+      cy.wrap(tc).eq(3).should('contain', 'smith')
+    })
+    // cy.get('thead').find('[ng-reflect-name="firstName"]').type('test name')
+    // cy.get('thead').find('[ng-reflect-name="lastName"]').type('last name')
+    // cy.get('thead').find('[ng-reflect-name="email"]').type('test@test.com')
+    // cy.get('thead').find('.nb-checkmark').click()
+
+    // cy.get('tbody').contains('tr', 'test name').then(tableRow => {
+    //   cy.wrap(tableRow).contains('last name')
+    //   cy.wrap(tableRow).contains('test@test.com')
+
+    // })
+
+
+
 
 
 })
